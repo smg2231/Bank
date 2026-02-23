404282f98e387e02da436b9e0e6d409b5f65553f
btbare <btbare2002@gmail.com>
2026-02-23 12:30:26 -0500

    add component to get bank balance

---
+++ app/admin/page.tsx
@@ -0,0 +1,7 @@
+import GetBankBalance from "@/components/bradley/BankBalance";
+
+export default function(){
+    <>
+    <GetBankBalance></GetBankBalance>
+    </>
+}
---
+++ components/bradley/BankBalance.tsx
@@ -0,0 +1,29 @@
+"use client"
+
+import {useState} from "react"
+
+export default function GetBankBalance() {
+
+    const urlBase = "https://695f03af7f037703a8128fbf.mockapi.io/api/v1/Account"
+
+    let [count, setCount] = useState(0)
+
+    var GetCount = async function(){
+        let req = await fetch(urlBase)
+        let res = await req.json()
+        for(let x = 0; x < res.length; x++){
+            setCount(Number(res[x].balance))
+        }
+    }
+
+    return(
+        <>
+            <button
+            type="button"
+            onClick={GetCount}
+            >Show bank balance</button>
+
+            (Total Cash in Bank: ${count})
+        </>
+    )
+}
